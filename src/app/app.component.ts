2da7678a539adb2c3286d8e17bdf1a784b697277
Carlos Trujillo <cacuevas@stefanini.com>
2021-11-07 21:46:45 -0300

    chore(todos): basic todo angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
+
+export interface ToDo {
+  id: number;
+  task: string;
+  complete: boolean;
+}
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-firebase';
+  private partialTodo: Partial<ToDo> = {};
+  completeToDos: Array<any> = [];
+  incompleteToDos: Array<any> = [];
+
+  constructor() {}
+
+  addToDo() {
+    const newToDo = {
+      ...this.partialTodo,
+      id: uuidv4(),
+      complete: false,
+    };
+    this.incompleteToDos.push(newToDo);
+  }
+
+  onAddToDoChange(toDo: Partial<ToDo>) {
+    this.partialTodo = toDo;
+  }
+
+  onCompleteToDo(toDo: ToDo) {
+    this.incompleteToDos = this.incompleteToDos.filter(
+      ({ id }) => id !== toDo.id
+    );
+    this.completeToDos.push({ ...toDo, complete: true });
+  }
+
+  onIncompleteToDo(toDo: ToDo) {
+    this.completeToDos = this.completeToDos.filter(({ id }) => id !== toDo.id);
+    this.incompleteToDos.push({ ...toDo, complete: false });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { TodoComponent } from './components/todo/todo.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TodoListComponent,
+    TodoFormComponent,
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSliderModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
